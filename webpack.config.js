ec14e8897962b2131353791bd477fc0596d5e8bd
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-08-08 07:56:38 -0400

    Fixed sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+
   module: {
     rules: [
       {
