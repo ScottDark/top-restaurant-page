705752d8baeea102dce9c3cfbfe27f2964ac9cfa
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-08-09 15:56:53 -0400

    Added event listeners to tabs

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,23 @@ import pageHome from "../components/page-home.js";
 import pageMenu from "../components/page-menu.js";
 import pageContact from "../components/page-contact.js";
 
+/* Add event listeners to tab switching */
+function addEventListenersToTab() {
+  const navTabs = document.querySelectorAll(".nav-link");
+
+  navTabs.forEach((tab) => {
+    tab.addEventListener("click", tabSwitching);
+  });
+}
+
+/* Dynamic tabs to switch between pages. */
+function tabSwitching() {
+  const selectContentWrapper = document.querySelector("#content");
+  console.log("I was clicked");
+}
+
 pageHome();
+addEventListenersToTab();
+
 // pageMenu();
 // pageContact();
