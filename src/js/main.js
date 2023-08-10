ebf3b6006b3dd178cf5edd9946a50a83da4aad61
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-08-10 08:18:04 -0400

    Added tab functionality to switch between pages

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,65 @@ import pageMenu from "../components/page-menu.js";
 import pageContact from "../components/page-contact.js";
 
 /* Add event listeners to tab switching */
-function addEventListenersToTab() {
+function tabSwitchingEventListeners() {
   const navTabs = document.querySelectorAll(".nav-link");
 
   navTabs.forEach((tab) => {
-    tab.addEventListener("click", tabSwitching);
+    tab.addEventListener("click", () => {
+      tabSwitching(tab);
+    });
   });
 }
 
 /* Dynamic tabs to switch between pages. */
-function tabSwitching() {
+function tabSwitching(tab) {
   const selectContentWrapper = document.querySelector("#content");
-  console.log("I was clicked");
+  const tabText = tab.innerText;
+  // console.log(tab.innerText + " was clicked!");
+
+  switch (tabText) {
+    case "Home":
+      unloadPage(tab);
+      pageHome();
+
+      break;
+
+    case "Menu":
+      unloadPage(tab);
+      pageMenu();
+
+      break;
+
+    case "Contact":
+      unloadPage(tab);
+      pageContact();
+
+      break;
+
+    default:
+      console.log("Error switching tabs.");
+      break;
+  }
 }
 
-pageHome();
-addEventListenersToTab();
+/* unload Page by removing all children of #content*/
+function unloadPage(tab) {
+  const selectContentWrapper = document.querySelector("#content");
+  const navTabs = document.querySelectorAll(".nav-link");
 
-// pageMenu();
-// pageContact();
+  // Remove all tab highlight "active" class.
+  navTabs.forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  // Set current tab as active.
+  tab.classList.add("active");
+
+  while (selectContentWrapper.firstChild) {
+    selectContentWrapper.removeChild(selectContentWrapper.firstChild);
+  }
+}
+
+// Initial load to home page.
+pageHome();
+tabSwitchingEventListeners();
